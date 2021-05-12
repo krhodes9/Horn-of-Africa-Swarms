60ec0e14d2e9b768e5132d697e25f91960e6d8ea
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-05-12 09:32:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-require([
-        "esri/Map",
-        "esri/layers/FeatureLayer",
-        "esri/layers/GeoJSONLayer",
-        "esri/views/MapView",
-         "esri/widgets/Legend",
-        "esri/widgets/Expand",
-        "esri/widgets/Home",
-        "esri/WebMap",
-      ], function(Map, FeatureLayer, GeoJSONLayer, MapView, Legend, Expand, Home, WebMap) {
+require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+        var map = new Map({
+          basemap: "dark-gray"
+        });
 
-          var featureLayer = new FeatureLayer({
-          url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/East_Africa/FeatureServer/0"
-        });
-  
-  const map = new Map({
-          basemap: "dark-gray",
-          layers: [featureLayer, layer]
-        });
-               
         var view = new MapView({
           container: "viewDiv",
-          map: map, 
-          center: [45, 9],
-          scale: 13340000
+          map: map,
+          zoom: 10,
+          center: [-90, 38] // longitude, latitude
         });
-  });
+      });
