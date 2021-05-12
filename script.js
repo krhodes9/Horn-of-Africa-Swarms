0e2d1cb94d5742211cc36458db56347f2395169a
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-05-12 09:27:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-  
 require([
         "esri/Map",
         "esri/layers/FeatureLayer",
@@ -13,75 +12,8 @@ require([
           var featureLayer = new FeatureLayer({
           url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/East_Africa/FeatureServer/0"
         });
-        
-        const clusterConfig = {
-          type: "cluster",
-          clusterRadius: "100px",
-          popupTemplate: {
-            content: "This cluster represents {cluster_count} urban trees.",
-            fieldInfos: [{
-              fieldName: "cluster_count",
-              format: {
-                places: 0,
-                digitSeparator: true
-              }
-            }]
-          },
-          clusterMinSize: "24px",
-          clusterMaxSize: "60px",
-          labelingInfo: [{
-            deconflictionStrategy: "none",
-            labelExpressionInfo: {
-              expression: "Text($feature.cluster_count, '#,###')"
-            },
-            symbol: {
-              type: "text",
-              color: "white",
-              font: {
-                weight: "bold",
-                family: "Noto Sans",
-                size: "12px"
-              }
-            },
-            labelPlacement: "center-center",
-          }]
-        }
-
-        var layer = new FeatureLayer({
-          title: "Desert Locust Swarms",
-          url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Swarms_East_Africa/FeatureServer",
-          copyright: "FAO",
-          featureReduction: clusterConfig,
-        
-
-          popupTemplate: {
-            title: "Desert Locust Info",
-            content: "Number {mag} {type} hit {place} on {time}",
-            fieldInfos: [
-              {
-                fieldName: "time",
-                format: {
-                  dateFormat: "short-date-short-time"
-                }
-              }
-            ]
-          },
-          renderer: {
-            type: "simple",
-            field:"",
-            symbol: {
-              type: "simple-marker",
-              size: 4,
-              color: "deeppink",
-              outline: {
-                color: "rgba(300, 0, 0, 0.4)",
-                width: 3
-              }
-            }
-          }
-        });
-
-        const map = new Map({
+  
+  const map = new Map({
           basemap: "dark-gray",
           layers: [featureLayer, layer]
         });
@@ -89,33 +21,7 @@ require([
         var view = new MapView({
           container: "viewDiv",
           map: map, 
-          center: [35.25,10.25],
-          scale: 134000
+          center: [45, 9],
+          scale: 13340000
         });
 
-        view.ui.add(new Home({
-          view: view
-        }), "top-left");
-
-       const legend = new Legend({
-          view: view,
-          container: "legendDiv"
-        });
-
-        const infoDiv = document.getElementById("infoDiv");
-        view.ui.add(new Expand({
-          view: view,
-          content: infoDiv,
-          expandIconClass: "esri-icon-layer-list",
-          expanded: false
-        }), "top-left");
-
-        const toggleButton = document.getElementById("cluster");
-
-        toggleButton.addEventListener("click", function(){
-          let fr = layer.featureReduction;
-          layer.featureReduction = fr && fr.type === "cluster" ? null : clusterConfig;
-          toggleButton.innerText = toggleButton.innerText === "Enable Clustering" ? "Disable Clustering" : "Enable Clustering";
-                  
-        });
-      });
